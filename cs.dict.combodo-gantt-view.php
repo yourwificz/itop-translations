53bc700ca97998fba3fb5d7f85e673e223e15bab
Martin Tesar <martin.tesar@yourwifi.cz>
2024-08-11 15:19:05 +0200

    CZ translation for Gantt view
    Initial commit

---
+++ cs.dict.combodo-gantt-view.php
@@ -0,0 +1,42 @@
+<?php
+/*
+ * @copyright   Copyright (C) 2010-2021 Combodo SARL
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+ /**
+ * @author Martin Tesař <martin.tesar@yourwifi.cz>
+ *
+ */
+ 
+Dict::Add('CS CZ', 'Czech', 'Čeština', array(
+	// Dictionary entries go here
+	'GanttDashlet/Label' => 'Gantt',
+	'GanttDashlet/Description' => 'See objects as a Gantt',
+	'GanttDashlet/NoPreviewAvailable' => 'No Preview Available!!',
+	'GanttDashlet/Prop:DefaultTitle' => 'Tickets',
+	'GanttDashlet/Prop:DefaultTitle2' => 'Contacts',
+
+	'GanttDashlet/Prop:Class' => 'Class',
+	'GanttDashlet/Prop:Name' => 'Name',
+	'GanttDashlet/Prop:StartDate' => 'Start date~~',
+	'GanttDashlet/Prop:EndDate' => 'End date~~',
+	'GanttDashlet/Prop:Title' => 'Title',
+	'GanttDashlet/Prop:DependsOn' => 'Depends on',
+	'GanttDashlet/Prop:ColoredField' => 'Colored by',
+	'GanttDashlet/Prop:AdditionalInfoLeft' => 'Additional information 1',
+	'GanttDashlet/Prop:AdditionalInfoRight' => 'Additional information 2',
+	'GanttDashlet/Attribute:ActionDisabled' => 'Action disabled for this object',
+	'GanttDashlet:UI/Tab:GanttViewer' => "Gantt View",
+	'GanttDashlet/Prop:ParentField' => 'Group by',
+	'GanttDashlet/Prop:Percentage' => 'Percentage of completion',
+	'GanttDashlet/Prop:GroupByInformations' => 'Group by informations',
+
+	'GanttDashlet/UI:CriticalPath' => 'Critical path',
+	'GanttDashlet/UI:ZoomIn' => 'Zoom in',
+	'GanttDashlet/UI:ZoomOut' => 'Zoom out',
+	'GanttDashlet/UI:MoveSplitBarRight' => 'Move Split Bar Right',
+	'GanttDashlet/UI:MoveSplitBarLeft' => 'Move Split Bar Left',
+	'GanttDashlet/UI:MoveSplitBarCenter' => 'Center Split Bar',
+
+	'GanttDashlet/Error:ParametersMissing' => 'Missing some parameters, please check the configuration.',
+));
